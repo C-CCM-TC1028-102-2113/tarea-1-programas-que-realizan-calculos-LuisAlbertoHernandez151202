1ed3d2b130b4aa194cb4abacc0d3dd6e3a6861ce
LuisAlbertoHernandez151202 <89438566+LuisAlbertoHernandez151202@users.noreply.github.com>
2021-08-24 13:17:47 -0500

    Update exercise.py

--- assignments/19Editorial/src/exercise.py
+++ assignments/19Editorial/src/exercise.py
@@ -1,7 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
-
-
+import math
+palabras=int(input("Dame el número de palabras:"))
+pagi=(math.ceil(palabras/475))
+total=pagi*50
+descuento=total*.10
+totalN=total-descuento
+totalN=round(totalN,1)
+print("El costo de la publicación es:"str(totalN))
+pass
 if __name__ == '__main__':
     main()
